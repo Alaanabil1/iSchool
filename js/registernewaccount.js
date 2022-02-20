bf7174bf5709e5b3c4990175c1a5ee83cbc99c1e
Alaanabil1 <86005804+Alaanabil1@users.noreply.github.com>
2022-02-20 02:10:56 +0200

    20-2

--- js/registernewaccount.js
+++ js/registernewaccount.js
@@ -4,3 +4,20 @@ $(".mobile-header .menu").click(function() {
 $(".mobile-header .close").click(function() {
     $(".mobile-header .menucontainer").hide("blind", { direction: "right" });
 })
+$("#countryInput1").click(function(){
+    $(".country-menu").slideToggle("fast")
+})
+$("#countryInput1").keyup(function () {
+   
+    var filter = $(this).val();
+    $(".country ul li").each(function () {
+        if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+            $(this).hide();
+        } else {
+            $(this).show()
+        }
+    });
+});
+$(".country ul li").click(function(){
+    $("#countryInput1").val($(this).html())
+})
